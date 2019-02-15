2853beee97dfb62a9691932d23c42e8695ae42df
Nick Benoit <nbenoit3@gmail.com>
2019-02-14 21:18:31 -0500

    added classes

--- index.js
+++ index.js
@@ -10,6 +10,46 @@ class Person {
 }
 
 
+class Card {
+    constructor(point, suit) {
+        this.point = point;
+        this.suit = suit;
+    }
+    getImageURL() {
+        return 'images/5_of_diamonds.png';
+    }
+}
+
+
+class Hand {
+    constructor(card) {
+        this.card = card;
+    }
+    addCard(card) {
+        this.card = [];
+        card.push(card);
+    }
+}
+
+class Deck {
+    constructor(numberOfCards) {
+        this.numberOfCards = numberOfCards;
+        var suits = ['diamonds', 'spades', 'clubs', 'hearts', 'star'];
+        var point = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+    }
+    draw() {
+        return
+
+    }
+    shuffle() {
+
+    }
+    numCardsLeft() {
+
+    }
+}
+
+
 var sonny = new Person("Sonny", "sonny@hotmail.com", '483-485-4948');
 var jordan = new Person("Jordan", "jordan@aol.com", "495-586-3456");
 
@@ -18,3 +58,9 @@ jordan.greet(sonny);
 
 console.log(sonny.email + sonny.phone);
 console.log(jordan.email + jordan.phone);
+
+
+
+let myCard = new Card(5, 'diamonds')
+console.log(myCard.point);
+console.log(myCard.suit);
